0aa7c810ce9d9d2e84ea53ec4a50a4f16f606d99
messat <148489126+messat@users.noreply.github.com>
2025-05-20 13:38:09 +0100

    This app adds readme file and functional console app on C#

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ToDoAppTaskTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      greetUser();
+
+      List<string> userList = new List<string> { "Set up treadmill", "Clean the bedroom" };
+
+      UserEntry();
+      UserSwitch(userList);
+
+    }
+
+    static void UserSwitch(List<string> userList)
+    {
+      string? input = Console.ReadLine();
+      char userRequest = !string.IsNullOrEmpty(input) ? input.Trim()[0] : ' ';
+      if (userRequest != 'q')
+      {
+        switch (userRequest)
+        {
+          case 'V':
+          case 'v':
+            ViewTaskBoard(userList);
+            break;
+          case 'A':
+          case 'a':
+            AddTaskBoard(userList);
+            break;
+          case 'D':
+          case 'd':
+            DeleteTaskBoard(userList);
+            break;
+          case 'U':
+          case 'u':
+            UpdateTaskBoard(userList);
+            break;
+          default:
+            Console.WriteLine("Try Again. Use the entries below only.".ToUpper() + "\n");
+            UserEntry();
+            UserSwitch(userList);
+            break;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Exited the app");
+      }
+
+    }
+
+    static void UserEntry()
+    {
+      Console.WriteLine("To Add a task - type \"A\" or \"a\"");
+      Console.WriteLine("To View all your task - type \"V\" or \"v\"");
+      Console.WriteLine("To Delete a task, type \"D\" or \"d\"");
+      Console.WriteLine("To Update a task, type \"U\" or \"u\"");
+      Console.WriteLine();
+      Console.WriteLine("Enter \"Q\" or \"q\" to exit the app");
+      Console.WriteLine();
+    }
+
+    static void AddTaskBoard(List<string> userList)
+    {
+      Console.WriteLine("Enter a new task:");
+      string? newTask = Console.ReadLine();
+      if (!string.IsNullOrWhiteSpace(newTask))
+      {
+        userList.Add(newTask);
+        Console.WriteLine($"Added \"{newTask}\" as a task");
+        Console.WriteLine();
+
+        UserEntry();
+        UserSwitch(userList);
+      }
+    }
+
+    static void DeleteTaskBoard(List<string> userList)
+    {
+      AllTasks(userList);
+      Console.WriteLine("Delete task number:");
+
+      try
+      {
+        int taskNum = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Task Deleted: {taskNum}. {userList[taskNum - 1]}");
+        Console.WriteLine();
+        userList.RemoveAt(taskNum - 1);
+      }
+      catch (ArgumentOutOfRangeException err)
+      {
+        Console.WriteLine(err.Message + "\n");
+        DeleteTaskBoard(userList);
+      }
+      catch (FormatException formatErr)
+      {
+        Console.WriteLine(formatErr.Message + "\n");
+        DeleteTaskBoard(userList);
+      }
+      UserEntry();
+      UserSwitch(userList);
+    }
+
+    static void ViewTaskBoard(List<string> userList)
+    {
+      Console.Clear();
+      Console.WriteLine("View All Tasks: \n");
+
+      AllTasks(userList);
+
+      UserEntry();
+      UserSwitch(userList);
+    }
+
+    static void greetUser()
+    {
+      Console.WriteLine("Welcome to Mo To Do App Tracker!".ToUpper());
+      Console.WriteLine();
+    }
+
+    static void UpdateTaskBoard(List<string> userList)
+    {
+      AllTasks(userList);
+      Console.WriteLine("Enter the numbered task to update:");
+      try
+      {
+        int taskNumUpdate = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Task to Update: {taskNumUpdate}. {userList[taskNumUpdate - 1]}");
+        Console.WriteLine();
+        userList.RemoveAt(taskNumUpdate - 1);
+        string? updateTask = Console.ReadLine();
+        userList.Insert(taskNumUpdate - 1, updateTask);
+        Console.WriteLine();
+        AllTasks(userList);
+        UserEntry();
+        UserSwitch(userList);
+      }
+      catch (ArgumentOutOfRangeException rangeErr)
+      {
+        Console.WriteLine(rangeErr.Message);
+        Console.WriteLine();
+        UpdateTaskBoard(userList);
+      }
+      catch (FormatException formatErr)
+      {
+        Console.WriteLine(formatErr.Message);
+        Console.WriteLine();
+        UpdateTaskBoard(userList);
+      }
+      catch (ArgumentNullException nullError)
+      {
+        Console.WriteLine(nullError.Message);
+        Console.WriteLine();
+        UpdateTaskBoard(userList);
+      }
+    }
+
+    static void AllTasks(List<string> userList)
+    {
+      for (int i = 0; i < userList.Count; i++)
+      {
+        Console.WriteLine($"{i + 1}. {userList[i]}");
+      }
+      Console.WriteLine();
+    }
+  }
+}
