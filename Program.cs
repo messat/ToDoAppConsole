8f7d7553479ec2b912d7e08bdb816f0e8966afa6
messat <148489126+messat@users.noreply.github.com>
2025-05-25 19:34:48 +0100

    This commit adds the streamwriter class to all CRUD operations ensuring ToDo App list is updated accurately.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 
 namespace ToDoAppTaskTracker
@@ -8,9 +9,10 @@ namespace ToDoAppTaskTracker
   {
     static void Main(string[] args)
     {
-      greetUser();
+      GreetUser();
+      FileCreation();
 
-      List<string> userList = new List<string> { "Set up treadmill", "Clean the bedroom" };
+      List<string> userList = new List<string>();
 
       UserEntry();
       UserSwitch(userList);
@@ -76,14 +78,22 @@ namespace ToDoAppTaskTracker
         Console.WriteLine($"Added \"{newTask}\" as a task");
         Console.WriteLine();
 
+        using (StreamWriter fileAppendTask = File.AppendText(@"TaskMemory.txt"))
+        {
+          int taskListCount = userList.Count;
+          fileAppendTask.WriteLine($"{taskListCount}. {newTask}");
+        }
+        AllTasks();
         UserEntry();
         UserSwitch(userList);
       }
     }
 
+
+
     static void DeleteTaskBoard(List<string> userList)
     {
-      AllTasks(userList);
+      AllTasks();
       Console.WriteLine("Delete task number:");
 
       try
@@ -92,6 +102,17 @@ namespace ToDoAppTaskTracker
         Console.WriteLine($"Task Deleted: {taskNum}. {userList[taskNum - 1]}");
         Console.WriteLine();
         userList.RemoveAt(taskNum - 1);
+        using (StreamWriter deleteTask = new StreamWriter(@"TaskMemory.txt"))
+        {
+          DateTime localDate = DateTime.Now;
+          deleteTask.WriteLine($"Tasks for today: {localDate.ToString("D")} \n");
+          for (int i = 0; i < userList.Count; i++)
+          {
+            deleteTask.WriteLine($"{i + 1}. {userList[i]}");
+          }
+        }
+        
+        
       }
       catch (ArgumentOutOfRangeException err)
       {
@@ -103,6 +124,7 @@ namespace ToDoAppTaskTracker
         Console.WriteLine(formatErr.Message + "\n");
         DeleteTaskBoard(userList);
       }
+      AllTasks();
       UserEntry();
       UserSwitch(userList);
     }
@@ -112,13 +134,13 @@ namespace ToDoAppTaskTracker
       Console.Clear();
       Console.WriteLine("View All Tasks: \n");
 
-      AllTasks(userList);
+      AllTasks();
 
       UserEntry();
       UserSwitch(userList);
     }
 
-    static void greetUser()
+    static void GreetUser()
     {
       Console.WriteLine("Welcome to Mo To Do App Tracker!".ToUpper());
       Console.WriteLine();
@@ -126,7 +148,7 @@ namespace ToDoAppTaskTracker
 
     static void UpdateTaskBoard(List<string> userList)
     {
-      AllTasks(userList);
+      AllTasks();
       Console.WriteLine("Enter the numbered task to update:");
       try
       {
@@ -135,9 +157,22 @@ namespace ToDoAppTaskTracker
         Console.WriteLine();
         userList.RemoveAt(taskNumUpdate - 1);
         string? updateTask = Console.ReadLine();
-        userList.Insert(taskNumUpdate - 1, updateTask);
+        if (updateTask is not null)
+        {
+          userList.Insert(taskNumUpdate - 1, updateTask);
+        }
         Console.WriteLine();
-        AllTasks(userList);
+        using (StreamWriter updateTaskFile = new StreamWriter(@"TaskMemory.txt"))
+        {
+          DateTime localDate = DateTime.Now;
+          updateTaskFile.WriteLine($"Tasks for today: {localDate.ToString("D")} \n");
+
+          for (int i = 0; i < userList.Count; i++)
+          {
+            updateTaskFile.WriteLine($"{i + 1}. {userList[i]}");
+          }
+        }
+        AllTasks();
         UserEntry();
         UserSwitch(userList);
       }
@@ -161,13 +196,35 @@ namespace ToDoAppTaskTracker
       }
     }
 
-    static void AllTasks(List<string> userList)
+    static void AllTasks()
     {
-      for (int i = 0; i < userList.Count; i++)
+      string? taskDuty;
+      try
       {
-        Console.WriteLine($"{i + 1}. {userList[i]}");
+        StreamReader viewAllTaskInList = new StreamReader("/Users/muhammadessat/Desktop/C#/practice/To do app /To Do App/TaskMemory.txt");
+        taskDuty = viewAllTaskInList.ReadLine();
+        while (taskDuty != null)
+        {
+          Console.WriteLine(taskDuty);
+          taskDuty = viewAllTaskInList.ReadLine();
+        }
+          viewAllTaskInList.Close();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Exception: " + e.Message);
       }
       Console.WriteLine();
+    }
+
+
+    static void FileCreation()
+    {
+      using (StreamWriter writeTasks = new StreamWriter(@"TaskMemory.txt"))
+      {
+        DateTime localDate = DateTime.Now;
+        writeTasks.WriteLine($"Tasks for today: {localDate.ToString("D")} \n");
+      }
     }
   }
 }
